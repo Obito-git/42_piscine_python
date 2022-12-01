64f6d539b955b1cb810661c050ab9c425ed3bb1d
amyroshn <12@d.d>
2022-12-01 11:52:06 +0100

    ex02/ex03

---
+++ module0/ex02/whois.py
@@ -0,0 +1,14 @@
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit("One integer arg expected")
+try:
+    res = int(sys.argv[1])
+except ValueError:
+    sys.exit(sys.argv[1] + " is not integer number")
+if res == 0:
+    print("I'm Zero")
+elif res % 2 == 0:
+    print("I'm Even")
+else:
+    print("I'm Odd")
